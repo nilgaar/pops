368ed3d7298cdaf54098d9b4cf12b76193e782b4
pops <gitkali.690x0@passmail.net>
2024-05-08 23:34:12 +0200

    `chore: Refactor spider.py for improved crawling logic`

--- web/spider.py
+++ web/spider.py
@@ -2,21 +2,26 @@ import argparse
 import requests
 import re
 
+alreadyCrawled = {""}
+
 
 def crawlForLinks(url):
+    print("baseUrl:", baseUrl)
+    alreadyCrawled.add(url)
     print(f"[*] Crawling {url}")
 
     page = requests.get(url)
+    mach = re.findall(r'href="(.*?)"', page.text)
 
-    maching = re.findall(r'href="(.*?)"', page.text)
-    for link in maching:
+    for link in mach:
         print(link)
+        if baseUrl in link and link not in alreadyCrawled:
+            crawlForLinks(link)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="List all links found in a web page"
-    )
+    global baseUrl
+    parser = argparse.ArgumentParser(description="List all links found in a web page")
     parser.add_argument("url", type=str, help="The URL of the webpage")
     baseUrl = parser.parse_args().url
     crawlForLinks(baseUrl)
