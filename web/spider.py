dc54230a1086e5b08fa7cd14a9ccbba1599c7f4d
pops <gitkali.690x0@passmail.net>
2024-05-08 18:36:07 +0200

    + spider.py

---
+++ web/spider.py
@@ -0,0 +1,26 @@
+import argparse
+import requests
+import re
+
+
+def crawlForLinks(url):
+    print(f"[*] Crawling {url}")
+
+    page = requests.get(url)
+
+    maching = re.findall(r'href="(.*?)"', page.text)
+    for link in maching:
+        print(link)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="List all domains and files in a webpage"
+    )
+    parser.add_argument("url", type=str, help="The URL of the webpage")
+    baseUrl = parser.parse_args().url
+    crawlForLinks(baseUrl)
+
+
+if __name__ == "__main__":
+    main()
